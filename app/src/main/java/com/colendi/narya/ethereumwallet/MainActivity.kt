72ddc2306479ae069f92b268c83656705641663e
ahmetcj4 <ahmetcj4@hotmail.com>
2018-10-23 02:12:44 +0300

    import kethereum lib

--- app/src/main/java/com/colendi/narya/ethereumwallet/MainActivity.kt
+++ app/src/main/java/com/colendi/narya/ethereumwallet/MainActivity.kt
@@ -2,8 +2,14 @@ package com.colendi.narya.ethereumwallet
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.colendi.narya.ethereumwallet.R.*
+import android.util.Log
+import com.colendi.narya.ethereumwallet.R.layout
 import kotlinx.android.synthetic.main.activity_main.*
+import org.kethereum.bip39.generateMnemonic
+import org.kethereum.bip39.model.MnemonicWords
+import org.kethereum.bip39.toKey
+import org.kethereum.bip39.validate
+import org.kethereum.bip39.wordlists.WORDLIST_ENGLISH
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,8 +18,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(layout.activity_main)
         print("here")
 
-        updateTextButton.setOnClickListener { welcomeTextView.text = "Hello Kotlin World!" }
-
+        updateTextButton.setOnClickListener {
+            val mnemonic = generateMnemonic(wordList = WORDLIST_ENGLISH)
+            welcomeTextView.text = mnemonic
+            Log.wtf("MainActivity", mnemonic)
+            val validate = MnemonicWords(mnemonic).validate(WORDLIST_ENGLISH)
+            Log.wtf("MainActivity", validate.toString())
+            val tokey=MnemonicWords(mnemonic).toKey(path = "m/44")
+            Log.wtf("MainActivity", tokey.toString())
+        }
     }
 
 }
